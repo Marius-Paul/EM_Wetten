2709fd4f1a9a78473f527e706eb1d751c27d0a92
paul_ <paul_marius@web.de>
2024-07-03 15:10:54 +0200

    03.07.24, 15:10

--- Main.py
+++ Main.py
@@ -20,7 +20,7 @@ for i in range(ng):
 
 
 
-def points(x, y, x0, y0):
+def points_rule1(x, y, x0, y0):
     distance = sqrt((x - x0) ** 2 + (y - y0) ** 2)
     if (x0>y0 and x>y) or (x0<y0 and x<y):
         points = 10 - distance**2
@@ -30,14 +30,38 @@ def points(x, y, x0, y0):
         points = 0
     return points
 
-total_points = zeros(nn, dtype=int)
+
+def points_rule2(x, y, x0, y0):
+    if x == x0 and y == y0:
+        points = 3.0
+    elif (x0==y0 and x==y) or (x0>y0 and x>y) or (x0<y0 and x<y):
+        points = 1.0
+    else:
+        points = 0.0
+    return points
+
+total_points_rule1 = zeros(nn, dtype=int)
 
 for j in range(nn):
     for i in range(ng_so_far):
-        points_of_person = points(xy_matrix[i, j, 0], xy_matrix[i, j, 1], real_results[i][0], real_results[i][1])
+        points_of_person = points_rule1(xy_matrix[i, j, 0], xy_matrix[i, j, 1], real_results[i][0], real_results[i][1])
         #print(xy_matrix[i, j, 0], xy_matrix[i, j, 1], ', ', real_results[i][0], real_results[i][1], ', ', points_of_person)
         #print('Points of ', names[j], ' in game ', i, ' is ', points_of_person, ' points!', '\n')
-        total_points[j] += round(points_of_person)
-    total_points[j] = total_points[j]
-    print(names[j], ' has ', total_points[j], ' points!')
-savetxt('total_points.txt', total_points)
+        total_points_rule1[j] += round(points_of_person)
+    total_points_rule1[j] = total_points_rule1[j]
+    print(names[j], ' has ', total_points_rule1[j], ' points!')
+savetxt('total_points_rule1.txt', total_points_rule1)
+
+
+print('\n')
+
+
+total_points_rule2 = zeros(nn, dtype=int)
+
+for j in range(nn):
+    for i in range(ng_so_far):
+        points_of_person = points_rule2(xy_matrix[i, j, 0], xy_matrix[i, j, 1], real_results[i][0], real_results[i][1])
+        total_points_rule2[j] += round(points_of_person)
+    total_points_rule2[j] = total_points_rule2[j]
+    print(names[j], ' has ', total_points_rule2[j], ' points!')
+savetxt('total_points_rule2.txt', total_points_rule2)
